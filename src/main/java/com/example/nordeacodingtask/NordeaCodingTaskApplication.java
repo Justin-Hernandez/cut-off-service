6cb1b853ad7cef5d1bb0dad01be50c9426717510
Justin Hernandez Jover <justin.hernandez@edu.uah.es>
2023-01-16 16:25:21 +0100

    Initial commit

---
+++ src/main/java/com/example/nordeacodingtask/NordeaCodingTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.nordeacodingtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NordeaCodingTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NordeaCodingTaskApplication.class, args);
+	}
+
+}
